a76b8bffa7d9713719a3e4d1707258d24d94efd8
sgm13555@gmail.com <sgm13555@gmail.com>
2025-09-16 16:30:50 -0500

    Back in action

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 3/Multiple Ifs/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 3/Multiple Ifs/task.py
@@ -1,14 +1,28 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+total = 0
 
 if height >= 120:
     print("You can ride the rollercoaster")
     age = int(input("What is your age? "))
     if age <= 12:
         print("Please pay $5.")
+        total = 5
     elif age <= 18:
         print("Please pay $7.")
+        total = 7
     else:
         print("Please pay $12.")
+        total = 12
+
+    wants_photo = input('Do you want a photo? (y/n): ')
+    if wants_photo == 'y':
+        print("That will be an additional $3")
+        total += 3
+    else:
+        print("Thank you!")
+
+    print(f"Your total is ${total}")
+
 else:
     print("Sorry you have to grow taller before you can ride.")
--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 3/Python Pizza/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 3/Python Pizza/task.py
@@ -1,5 +1,32 @@
 print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M or L: ")
-pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
-extra_cheese = input("Do you want extra cheese? Y or N: ")
+size = input("What size pizza do you want? S, M or L: ").upper()
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ").upper()
+extra_cheese = input("Do you want extra cheese? Y or N: ").upper()
+total = 0
 
+# Small pizza (S): $15
+# Medium pizza (M): $20
+# Large pizza (L): $25
+# Add pepperoni for small pizza (Y or N): +$2
+# Add pepperoni for medium or large pizza (Y or N): +$3
+# Add extra cheese for any size pizza (Y or N): +$1
+
+if size == "S":
+    total = 15
+elif size == "M":
+    total = 20
+elif size == "L":
+    total = 25
+else:
+   print("Please enter a valid size.")
+
+if pepperoni == "Y":
+    if size == "S":
+        total +=2
+    else:
+        total +=3
+
+if extra_cheese == "Y":
+    total +=1
+
+print(f"Your final bill is: ${total}.")
