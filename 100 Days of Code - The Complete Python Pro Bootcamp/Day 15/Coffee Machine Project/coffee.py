3bf831af0d65f8f2f4a3e4bdf9b96521dc471aa7
sgm13555@gmail.com <sgm13555@gmail.com>
2026-01-27 16:27:53 -0600

    Coffee Machine

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 15/Coffee Machine Project/coffee.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 15/Coffee Machine Project/coffee.py
@@ -37,3 +37,17 @@ def resource_amount(order_ingredients):
             print(f"Sorry, not enough of {item} for the drink.")
             is_enough =  False
     return is_enough
+
+def process_coins():
+    print("Please insert coins.")
+    quarters = int(input("How many quarters? ")) * 0.25
+    dimes = int(input("How many dimes? ")) * 0.1
+    nickles = int(input("How many nickles? ")) * 0.05
+    pennies = int(input("How many pennies? ")) * 0.01
+    total = quarters + dimes + nickles + pennies
+    return total
+
+def make_drink(drink_ingredients):
+    for item in drink_ingredients:
+        resources[item] -= drink_ingredients[item]
+
--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 15/Coffee Machine Project/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 15/Coffee Machine Project/main.py
@@ -1,3 +1,5 @@
+from traceback import print_tb
+
 import coffee as cf
 
 menu = cf.MENU
@@ -5,7 +7,7 @@ resources = cf.resources
 
 # control state of machine
 is_on = True
-money = 0
+profit = 0
 
 while is_on:
     choice = input("What would you like? (espresso/latte/cappuccino): ")
@@ -18,10 +20,28 @@ while is_on:
         print(f'Water: {resources['water']} ml\n'
               f'Milk: {resources['milk']} ml\n'
               f'Coffee: {resources['coffee']} ml\n'
-              f'Money: ${money}')
+              f'Money: ${profit}')
     else:
         # get resources required for selected drink
         drink = menu[choice]
+        print(drink)
+        print(choice)
         #  check if there is enough via function
         if cf.resource_amount(drink['ingredients']):
-            print('Thanks')
+            payment = cf.process_coins()
+            while payment < drink['cost']:
+                print(f"{payment}. Please add more coins")
+                add_coins = input("Would you like to add more coins? (yes/no): ")
+                if add_coins == "no":
+                    print(f"Your payment of {payment} has been refunded refunded.")
+                    exit()
+                else:
+                    payment += cf.process_coins()
+            print(f'You payed: {payment}')
+
+            change_owed = round(payment - drink['cost'],2)
+            print(f'Your change is {change_owed}.')
+            profit += drink['cost']
+
+            cf.make_drink(drink['ingredients'])
+            print(f"{choice} is now available.")
